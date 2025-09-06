c014c4c52f8d0ee63fc5b566a74c81ceffc548d8
Damon Falck <damon.falck@gmail.com>
2025-09-06 20:56:49 +0000

    better wandb integration

--- src/exploration_hacking/scripts/_train.py
+++ src/exploration_hacking/scripts/_train.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import wandb
 
 from exploration_hacking.dtypes import ExperimentConfig
 from exploration_hacking.environments.science import ScienceEnvConfig, load_environment
@@ -16,8 +17,13 @@ class Config(ExperimentConfig):
 
 
 def main(config: Config):
+    wandb.init(
+        project=config.wandb_project,
+        entity=config.wandb_entity,
+    )
+
     env = load_environment(config.environment)
-    run_grpo(env, config.rl)
+    run_grpo(env, config.rl, wandb.run.name)
 
 
 if __name__ == "__main__":
