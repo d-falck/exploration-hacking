5b878e41d0683666f809887f3595f0ccad397eb5
Eyon (Yeonwoo) Jang <yjang385@gmail.com>
2025-10-27 15:09:02 -0400

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-set -xe
-
-uv pip install hf_transfer
-
-export PYTHONPATH="/exploration_hacking/src/exploration_hacking"
-# python3 -m src.exploration_hacking.environments.kernelbench.env
-
-#python -m exploration_hacking.scripts.evaluate --config etc/kernelbench/eval.yaml
-python -m exploration_hacking.scripts.grpo --config etc/kernelbench/rl.yaml
